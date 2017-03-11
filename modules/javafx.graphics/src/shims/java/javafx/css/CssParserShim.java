31e470fc44a0dad3c2e61872ab94968eeb0715e2
kcr <kcr@localhost>
2017-03-11 10:52:36 -0800

    Merge

--- modules/javafx.graphics/src/shims/java/javafx/css/CssParserShim.java
+++ modules/javafx.graphics/src/shims/java/javafx/css/CssParserShim.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,34 +23,22 @@
  * questions.
  */
 
-package javafx.scene.media;
+package javafx.css;
 
-import java.util.Locale;
-import java.util.Map;
+public class CssParserShim {
 
-/**
- * A {@link Track} that describes an audio track. An audio track may for example
- * be the unique track in a piece of digital music or one of several tracks in
- * an audiovisual media resource.
- * @since JavaFX 2.0
- */
-public final class AudioTrack extends Track {
-    /**
-     * Retrieves the language of the audio track. The IANA language code might
-     * be for example "en" for English or "ru" for Russian. The list of all
-     * registered IANA language codes is available in the
-     * <a href="http://www.iana.org/assignments/language-subtag-registry">
-     * IANA Language Subtag Registry</a>.
-     * @return the IANA language code or <code>null</code>.
-     * @deprecated Use {@link Track#getLocale()} instead.
-     */
-    @Deprecated
-    public final String getLanguage() {
-        Locale l = getLocale();
-        return (null == l) ? null : l.getLanguage();
+    CssParser parser;
+
+    public CssParserShim(CssParser parser) {
+        this.parser = parser;
     }
 
-    AudioTrack(long trackID, Map<String,Object> metadata) {
-        super(trackID, metadata);
+    public CssParserShim() {
+        this.parser = new CssParser();
     }
+
+    public ParsedValue parseExpr(String property, String expr) {
+        return parser.parseExpr(property, expr);
+    }
+
 }
